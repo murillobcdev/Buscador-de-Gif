fd67a4d442067b4e14b7f68a5546e3daab9c32c7
Murillo Brito <murillobdev@gmail.com>
2021-11-09 14:40:27 -0300

    alteraçoes nas funçoes, coloquei a maioria do codigo em um eventlistener

--- script.js
+++ script.js
@@ -4,28 +4,24 @@ const $ = (e) => document.querySelector(e);
 const $s = (e) => document.querySelectorAll(e);
 
 $("#term").addEventListener('keyup', () => {
-    let term = $("#term").value;
-    console.log("soltou tecla e pesquisou por: " + term)
-    findGif(term, apikey);
-});
 
-function findGif(term, apikey) {
+    let term = $("#term").value;
+
+    console.log("soltou tecla e pesquisou por: " + term)
+
     fetch(`https://g.tenor.com/v1/search?q=${term}&key=${apikey}&limit=25`)
         .then(response => response.json())
         .then(json => {
-            montarGif(json);
+            let resultados = json['results'];
+            console.log(resultados);
+            $(".row").innerHTML = "";
+            for (let i in resultados) {
+                $(".row").innerHTML +=
+                    `<div class="gifCol col-2 p-3">
+                        <p class="gifTitle">${resultados[i].content_description}</p>
+                        <img src="${resultados[i]['media'][0]['gif']['url']}" class="gifImage img-fluid"/>
+                    </div>`;
+            };
         })
-};
+});
 
-function montarGif(json) {
-    let resultados = json['results'];
-    console.log(resultados);
-    $(".row").innerHTML = "";
-    for (let i in resultados) {
-        $(".row").innerHTML +=
-            `<div class="gifCol col-2 p-3">
-                <p class="gifTitle">${resultados[i].content_description}</p>
-                <img src="${resultados[i]['media'][0]['gif']['url']}" class="gifImage img-fluid"/>
-            </div>`;
-    }
-}
