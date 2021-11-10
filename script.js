bb516be9f3f48ea64f1b97d7a72d6d37cbdffee3
Murillo Brito <murillobdev@gmail.com>
2021-11-10 12:54:08 -0300

    alterações no layout e um pouco de responsividade

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 
 const apikey = "RQCGE0L9XC7M"
+
 const $ = (e) => document.querySelector(e);
 const $s = (e) => document.querySelectorAll(e);
 
 $("#term").addEventListener('keyup', () => {
-
     let term = $("#term").value;
-
-    console.log("soltou tecla e pesquisou por: " + term)
-
-    fetch(`https://g.tenor.com/v1/search?q=${term}&key=${apikey}&limit=25`)
+    fetch(`https://g.tenor.com/v1/search?q=${term}&key=${apikey}&limit=30`)
         .then(response => response.json())
         .then(json => {
             let resultados = json['results'];
-            console.log(resultados);
-            $(".row").innerHTML = "";
+            $(".row").innerHTML = ``;
             for (let i in resultados) {
                 $(".row").innerHTML +=
-                    `<div class="gifCol col-2 p-3">
+                    `
+                <div class="col-6 my-3">
+                    <div class="text-center">
                         <p class="gifTitle">${resultados[i].content_description}</p>
+                    </div>
+                    <div class="gifBody text-center">
                         <img src="${resultados[i]['media'][0]['gif']['url']}" class="gifImage img-fluid"/>
-                    </div>`;
+                    </div>
+                </div>
+                    `;
             };
         })
 });
 
+/* <p class="gifTitle">${resultados[i].content_description}</p>
+<img src="${resultados[i]['media'][0]['gif']['url']}" class="img-fluid"/> */
